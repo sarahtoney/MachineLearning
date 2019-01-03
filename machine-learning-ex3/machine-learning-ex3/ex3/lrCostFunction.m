6ddd9e1d75eb869528fa76ea39e36448113a080d
sarahtoney <34214659+sarahtoney@users.noreply.github.com>
2019-01-03 19:12:45 +0530

    Add files via upload

---
+++ machine-learning-ex3/machine-learning-ex3/ex3/lrCostFunction.m
@@ -0,0 +1,64 @@
+function [J, grad] = lrCostFunction(theta, X, y, lambda)
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%regularization
+%   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Hint: The computation of the cost function and gradients can be
+%       efficiently vectorized. For example, consider the computation
+%
+%           sigmoid(X * theta)
+%
+%       Each row of the resulting matrix will contain the value of the
+%       prediction for that example. You can make use of this to vectorize
+%       the cost function and gradient computations. 
+%
+% Hint: When computing the gradient of the regularized cost function, 
+%       there're many possible vectorized solutions, but one solution
+%       looks like:
+%           grad = (unregularized gradient for logistic regression)
+%           temp = theta; 
+%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           grad = grad + YOUR_CODE_HERE (using the temp variable)
+%
+n = length(theta);
+theta1n = theta(2:n);
+
+#Unregualrized cost function
+h = sigmoid(X*theta);
+J0 = y' * log(h);
+J1 = (1 - y)' * log(1 - h);
+JUnregularized = (-1/m)*(J0 + J1);
+
+#Regularized cost function
+
+theta1nSquared = theta1n' * theta1n;
+regularizationCost = (lambda/(2*m))*theta1nSquared;
+J = regularizationCost + JUnregularized;
+
+grad = (1/m)*(X' *(h - y));
+grad(2:n) = grad(2:n) + (lambda/m)*theta1n; 
+
+
+
+
+
+
+
+% =============================================================
+
+
+
+end
